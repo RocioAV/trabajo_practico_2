f73a1f9acb5eb0cb7c087c38698e81819112e36d
RocioAV <44351449@fi.unju.edu.ar>
2024-04-24 16:10:05 -0300

    Ejercicio3 completo - desarrollo del Main

--- tp2_VelazquezRocio/src/ar/edu/unju/fi/ejercicio3/main/Main.java
+++ tp2_VelazquezRocio/src/ar/edu/unju/fi/ejercicio3/main/Main.java
@@ -1,9 +1,17 @@
 package ar.edu.unju.fi.ejercicio3.main;
 
+import ar.edu.unju.fi.ejercicio3.constantes.Provincia;
+
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		Provincia[] provincias  = Provincia.values();
+		
+		for (Provincia provincia : provincias) {
+			System.out.println(provincia + ": - POBLACION= " + provincia.getCANT_POBLACION()+ " - SUPERFICE= "+ provincia.getSUPERFICIE()+ " km2 - DENSIDAD POBLACIONAL= "+ provincia.densidadPoblacion()+ " hab/km2");
+		}
+		
 
 	}
 
